b9c98ace6987233b48fcd81ccb921e292d7250d9
Ibrahem Youssef <67711816+Ibrahemyoussef2020@users.noreply.github.com>
2020-09-04 20:30:14 +0200

    up-sharp

---
+++ js/sharp.js
@@ -0,0 +1,12 @@
+/*
+function asideTranslate() {
+
+	document.getElementById('fixed-bar').style.display = none;
+
+}
+ */	
+    function asideTranslate()  {
+
+	 document.getElementById("fixed-bar").style.transform = "translate(0,0)scale(1.1,1)";
+	
+}
